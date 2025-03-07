7e16c182546a27bc93e10503f73903ae14436321
Mohsin12 <mohsin79.kh@gmail.com>
2025-03-07 16:16:24 +0530

    app.py file added

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-"This is new demo"
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return "Welcome to the mohsin home page"
+
+@app.route("/index")
+def index():
+    return "This is our home page"
+
+if __name__=="__main__":
+    app.run(debug=True)
